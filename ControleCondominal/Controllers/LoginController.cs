0585f028a95f979e208ff14fe1581c8dfe71916e
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2023-11-23 20:01:40 -0300

    inicio da tela home

---
+++ ControleCondominal/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleCondominal.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Login";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string username, string password)
+        {
+
+            if (username == "adm" && password == "123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Usuário ou senha inválidos.");
+                return View();
+            }
+        }
+    }
+    
+}
