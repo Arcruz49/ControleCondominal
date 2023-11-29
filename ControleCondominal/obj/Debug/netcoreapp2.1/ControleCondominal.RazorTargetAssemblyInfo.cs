60f4095859ab5928fc8e4298a50d787d49b11c3d
Arthur Cruz <arthur.cruz@objetivasolucao.com.br>
2023-11-28 22:34:23 -0300

    modificaçoes no layout

--- ControleCondominal/obj/Debug/netcoreapp2.1/ControleCondominal.RazorTargetAssemblyInfo.cs
+++ ControleCondominal/obj/Debug/netcoreapp2.1/ControleCondominal.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleCondominal")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7939777c6f755ab46394e50affcf226f1e4ce658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleCondominal.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
